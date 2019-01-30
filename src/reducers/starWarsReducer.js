df68a152330089f0b1aaf017b30defdd75086f43
Henry Neal <henry.neal0@gmail.com>
2019-01-30 16:27:39 -0500

    character names are rendering, working on grabbing more data from the api then onto stretch

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,6 +1,9 @@
-import /* we need our action types here*/ "../actions";
+import {SWAPI_FETCHING, SWAPI_SUCCESS, SWAPI_FAILURE} from "../actions";
+
 const initialState = {
-  characters: []
+  characters: [],
+  error:'',
+  isFetching: false
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
@@ -8,6 +11,28 @@ export const charsReducer = (state = initialState, action) => {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case SWAPI_FETCHING:
+      return {
+        ...state,
+        error:'',
+        isFetching:true
+      };
+    case SWAPI_SUCCESS:
+      return {
+        ...state, 
+        
+        characters: action.payload,
+        isFetching:false,
+        error: ''
+      };
+
+    case SWAPI_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        isFetching: false
+      }
+    
     default:
       return state;
   }
