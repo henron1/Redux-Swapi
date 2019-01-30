6ebc1486b81718823026e511671c4b0c8b4ddffa
Henry Neal <henry.neal0@gmail.com>
2019-01-30 15:31:00 -0500

    added actions and imports

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,13 @@
+import axios from 'axios';
 // we'll need axios
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
+export const SWAPI_FETCHING = 'SWAPI_FETCHING';
+export const SWAPI_SUCCESS = 'SWAPI_SUCCESS';
+export const SWAPI_FAILURE = 'SWAPI_FAILURE';
+
+
 
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
